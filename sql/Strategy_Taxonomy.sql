9db49922af90228784e187e84686334a092d18fe
echasin <echasin@innvosolutions.com>
2016-06-13 00:08:55 -0400

    Add folder to store test sql

---
+++ sql/Strategy_Taxonomy.sql
@@ -0,0 +1,4 @@
+﻿Select s.name As name, s.description as description, t.name as tag, s.status, s.lastmodifiedby, s.lastmodifieddatetime, s.domain
+from Strategy s, tagmbr tm, tag t
+where s.id = tm.strategytagmbrlhs_id
+and tm.tagrhs_id = t.id
