38abef58fee28d382aa21ac50e1f3f124b85ce5a
echasin <echasin@innvosolutions.com>
2017-01-22 23:00:36 -0800

    Updated objectype enumerations Updated database to adap_mrk Loads MRK specific csv files

--- src/main/java/com/innvo/domain/enumeration/Objecttype.java
+++ src/main/java/com/innvo/domain/enumeration/Objecttype.java
@@ -4,5 +4,5 @@ package com.innvo.domain.enumeration;
  * The Objecttype enumeration.
  */
 public enum Objecttype {
-    Asset,Organization,Person,Location,Event,Project,Portfolio,Activity,Scenario,Pathway
+    Asset,Organization,Person,Location,Event,Project,Portfolio,Activity,Scenario,Pathway,Countermeasure,Weapon
 }
